664813344625b63ff0cbb2a6a9c11b44678bc78b
MariiaAmbr <soul8888@gmail.com>
2022-11-30 21:05:03 +0200

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,16 +4,16 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Mariia and is{" "}
-        <a
-          href="https://github.com/MariiaAmbr/weather-react-project"
-          target="_blank"
-        >
-          open-soursed on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+
+        <footer>
+          This project was coded by Mariia and is{" "}
+          <a href="https://github.com/MariiaAmbr/weather-react-project">
+            open-soursed on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
